86b5059d0d8c3af09b10127fb1f1cac8159d6407
Muskan <muskanjain7206@gmail.com>
2026-07-04 19:47:07 +0530

    first commit

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="content">
+        <div className="title">
+          <h1>LET'S</h1>
+          <h1>GET</h1>
+          <h1>MOVING</h1>
+        </div>
+        <div className="sub-title">
+          <p>Your Journey to Fitness Starts Here</p>
+          <p>Unleash Your Potential</p>
+        </div>
+        <div className="buttons">
+          <a href="#contact"><button>Start Your Journey</button></a>
+          <a href="#pricing">          <button>Discover Your Plan</button>
+</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
